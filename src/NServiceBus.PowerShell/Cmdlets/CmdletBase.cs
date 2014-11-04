275d0337768737cae9c3f6e9f1173999ac35f854
Greg Bielleman <greg.bielleman@particular.net>
2014-11-05 10:45:38 +1100

    Simplify error

--- src/NServiceBus.PowerShell/Cmdlets/CmdletBase.cs
+++ src/NServiceBus.PowerShell/Cmdlets/CmdletBase.cs
@@ -11,8 +11,8 @@
             var processUtil = new ProcessUtil(Host);
             if (!processUtil.IsRunningWithElevatedPrivileges())
             {
-                var exception = new SecurityException("NServiceBus was unable to perform some infrastructure operations. You need to run this command with elevated privileges. If you are running this command from Visual Studio please close Visual Studio and re-open with elevated privileges. For more information see: http://particular.net/articles/preparing-your-machine-to-run-nservicebus");
-                ThrowTerminatingError(new ErrorRecord(exception, "NotAuthorized", ErrorCategory.SecurityError, null));
+                var exception = new SecurityException("This command requires elevated privileges");
+                ThrowTerminatingError(new ErrorRecord(exception, null, ErrorCategory.PermissionDenied, null));
             }
         }
     }
